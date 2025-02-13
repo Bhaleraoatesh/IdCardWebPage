6ec2c601c0370ee669d7d89ce49e9cca9fcbef8b
atesh bhalerao <bhalerao1214@gmail.com>
2025-02-13 17:11:06 +0530

    contact fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Customization from "./components/Customization";
-import Order from "./components/Order";
+import Contact from "./components/Contact";
 import About from "./components/About";
 import FAQ from "./components/FAQ";
 
@@ -18,7 +18,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/customization" element={<Customization />} />
-                <Route path="/order" element={<Order />} />
+                <Route path="/Contact Us" element={<Contact />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/faq" element={<FAQ />} />
             </Routes>
